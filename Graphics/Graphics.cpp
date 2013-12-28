90857709bb150485cd438f212fd50a594569e2b6
Nicolas-Imp <nicolaskagami@gmail.com>
2013-12-28 20:41:21 -0200

    Graphics

--- Graphics/Graphics.cpp
+++ Graphics/Graphics.cpp
@@ -3,7 +3,21 @@
 
 int main( int argc, char* args[] )
 {
+    SDL_Surface * hello = NULL;
+    SDL_Surface * screen = NULL;
     SDL_Init( SDL_INIT_EVERYTHING );
+
+    screen = SDL_SetVideoMode(600,400,8,SDL_SWSURFACE);
+
+    hello = SDL_LoadBMP("BMPTeste.bmp");
+
+    SDL_BlitSurface (hello, NULL, screen, NULL);
+
+    SDL_Flip( screen);
+
+    SDL_Delay(2000);
+
+    SDL_FreeSurface(hello);
 
     SDL_Quit();
 
